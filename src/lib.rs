0d4881416c7def3a118ced0963d8f132ae8b8a1e
Ron Fox <fox@frib.msu.edu>
2026-01-15 08:44:13 -0500

    Define the DataSource trait all datas sources must implement

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 pub mod online;
 pub mod offline;
+use rust_ringitem_format;
 
+pub trait DataSource {
+    fn open(&mut self, uri: &str) -> Result<Box<dyn DataSource>, String>;
+    fn read(&mut self) -> Option<rust_ringitem_format::RingItem>;
+}
