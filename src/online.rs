856998af853da23b3ccc809eaaa512abdcc2316f
Ron Fox <fox@frib.msu.edu>
2026-01-15 10:52:52 -0500

    Coded the tcp data source DataSource open trait method.

--- src/online.rs
+++ src/online.rs
@@ -1,16 +1,22 @@
 use rust_ringitem_format;
 use nscldaq_ringbuffer;
+use ringmaster_client;
+
 use crate::DataSource;
 use url::{Url, ParseError};
-// Stub for online data source:
 
+/// This represents an online data source that is
+/// potentially remote.  At thist time, rather than reading
+/// blocks of data, we read one ring item at a time.
+/// In the future, we may want to read blocks of data and unpack them like we do for files.
+/// 
 pub struct TcpDataSource {
-
+    ring : Option<ringmaster_client::RingBufferConsumer>,
 }
 impl TcpDataSource {
     pub fn new() -> TcpDataSource { 
         TcpDataSource {
-
+            ring : None
         }
 
     }
@@ -26,7 +32,12 @@ impl DataSource for TcpDataSource {
         if url.scheme() != "tcp" {
             return Err(format!("Invalid URI scheme for TcpDataSource: {}", url.scheme()));
         }
-        Ok(())  
+        let mut ring = ringmaster_client::RingBufferConsumer::attach(uri);
+        if let Err(e) = ring {
+            return Err(format!("Failed to connect to ring buffer at {}: {}", uri, e));  
+        }
+        self.ring = Some(ring.unwrap());
+        Ok(())
     }
     
     fn read(&mut self) -> Option<rust_ringitem_format::RingItem> {
